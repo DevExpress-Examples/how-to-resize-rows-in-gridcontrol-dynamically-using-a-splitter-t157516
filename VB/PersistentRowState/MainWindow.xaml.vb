1c69e78721d1304d46de5a9b6cdb4aa30e67c169
DevExpressExampleBot <fakeEmail>
2025-07-24 19:08:25 +0400

    Source auto update [skip ci]

--- VB/PersistentRowState/MainWindow.xaml.vb
+++ VB/PersistentRowState/MainWindow.xaml.vb
@@ -1,31 +1,20 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace PersistentRowState
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			grid.ItemsSource = Data.GetDataList(100)
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.grid.ItemsSource = Data.GetDataList(100)
+        End Sub
+    End Class
 End Namespace
